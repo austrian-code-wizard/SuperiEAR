2cdebd7dc044357ad403ccf5dfd0ab5ba252ce68
Moritz Stephan <moritz.stephan@stanford.edu>
2022-11-23 10:50:44 -0800

    added eval set generation

--- generate_data.py
+++ generate_data.py
@@ -1,5 +1,5 @@
 from superiear.scraping import download_audio, read_urls
-from superiear.processing import split_sentences, insert_noise
+from superiear.processing import split_sentences, insert_noise, split_test_samples
 
 data_urls = read_urls("data_urls.json")
 download_audio(data_urls, "./data/raw")
@@ -7,5 +7,6 @@ download_audio(data_urls, "./data/raw")
 noise_urls = read_urls("noise_urls.json")
 download_audio(noise_urls, "./data/noises")
 
-split_sentences("./data/raw", "./data/processed")
-insert_noise("./data/processed", "./data/final", "./data/noises/")
+split_sentences("./data/raw", "./data/clear_samples")
+insert_noise("./data/clear_samples", "./data/noisy_samples", "./data/noises/")
+split_test_samples("./data/clear_samples", "./data/noisy_samples")
